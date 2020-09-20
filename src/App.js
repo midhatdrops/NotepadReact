d39fdcb613b11a7e117073f77d1cf6ed0450296a
Bruno R.Novais <novais.rezende@gmail.com>
2020-09-20 16:52:04 -0300

    Notepad query created, need some review and css styles. Need to create delete button too

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import './styles.css'
 
-function App() {
+
+export default function App() {
+  const useStored = JSON.parse(window.localStorage.getItem('notes'))
+  let [notes, setNotes] = useState(useStored)
+  if(notes === null ) (notes = [])
+
+
+  function Salvar() {
+      window.localStorage.removeItem('notes')
+      const text = document.querySelector('#note_text').value
+      const dia = Date.now()
+      let id = 0 
+      notes.length === 0 ? id = 0 : id = notes.length
+      const att = []
+      att.push(...notes,{text,dia, id})
+      window.localStorage.setItem('notes', JSON.stringify(att))
+      console.log(att)
+      return setNotes(att)
+    }
+
+    function Delete() {
+      window.localStorage.removeItem('notes')
+      setNotes([])
+      return console.log('Local storage deletado')
+    }
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="back">
+  <ul>
+  {notes.map( note => (
+    <li key={note.dia}>{note.text}</li>
+  )
+  )}
+    <input id="note_text"></input>
+    <button onClick={() => Salvar()}>Salvar</button>
+    <button onClick={() => Delete()}>Deletar</button>
+  </ul>
     </div>
   );
 }
 
-export default App;
+
+
